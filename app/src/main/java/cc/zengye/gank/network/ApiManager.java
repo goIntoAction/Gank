d483dc44d115dc9010e931da7ebe6dfa77358c47
曾晔 <zengye@meizu.com>
2016-06-07 20:12:57 +0800

    开发中。。。

--- app/src/main/java/cc/zengye/gank/network/ApiManager.java
+++ app/src/main/java/cc/zengye/gank/network/ApiManager.java
@@ -1,12 +1,39 @@
 package cc.zengye.gank.network;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+import cc.zengye.gank.network.api.GankApi;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
 /**
  * Created by fanny on 16/6/5.
  */
 public class ApiManager {
+    final Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").serializeNulls().create();
+    final GankApi mGankApi;
 
     private ApiManager () {
-
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .connectTimeout(15,TimeUnit.SECONDS)
+                .retryOnConnectionFailure(true).build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .client(client)
+                .baseUrl("http://api.gank.io/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build();
+        mGankApi = retrofit.create(GankApi.class);
     }
 
     public ApiManager getInstance(){
@@ -15,5 +42,9 @@ public class ApiManager {
 
     static class Holder {
         private static ApiManager INSTANCE = new ApiManager();
+    }
+
+    public GankApi getGankApi() {
+        return mGankApi;
     }
 }
--- app/src/main/java/cc/zengye/gank/ui/adapter/GankMainAdapter.java
+++ app/src/main/java/cc/zengye/gank/ui/adapter/GankMainAdapter.java
@@ -1,4 +1,4 @@
-package cc.zengye.gank.adapter;
+package cc.zengye.gank.ui.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
